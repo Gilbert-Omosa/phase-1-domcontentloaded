aefb166d4208e7fb290fca7695289676468cf083
Gilbert Omosa <gilbert.omosa@student.moringaschool.com>
2023-09-29 20:42:57 +0300

    Done With Assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+
+  const paragraphElement = document.getElementById("text");
+  paragraphElement.textContent = "This is really cool!";
+
+});
+
+console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+  );
